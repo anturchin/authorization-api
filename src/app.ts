0dd59489d216f6bba002482516443a608d895f84
anturchin <tur4intwo@gmail.com>
2023-12-25 13:24:36 +0300

    feat: add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, { Express } from 'express';
-import { userRouter } from './users/users';
 import { Server } from 'http';
 import { LoggerService } from './logger/logger.service';
+import { UserController } from './users/users.controller';
+import { ExceptionFilter } from './errors/exception.filter';
 
 export class App {
 
@@ -9,19 +10,32 @@ export class App {
 	server: Server;
 	port: number;
 	logger: LoggerService;
+	userController: UserController;
+	exceptionFilter: ExceptionFilter;
 
-	constructor(logger: LoggerService) {
+	constructor(
+		logger: LoggerService,
+		userController: UserController,
+		exceptionFilter: ExceptionFilter,
+	) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
+		this.userController = userController;
+		this.exceptionFilter = exceptionFilter;
 	}
 
 	useRoutes() {
-		this.app.use('/users', userRouter);
+		this.app.use('/users', this.userController.router);
+	}
+
+	useExceptionFilter() {
+		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
 	}
 
 	public async init() {
 		this.useRoutes();
+		this.useExceptionFilter();
 		this.server = this.app.listen(this.port);
 		this.logger.log(`server is running on port ${this.port}`);
 	}
